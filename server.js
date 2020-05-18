ff3c3388007eca78a9d51d057eb72d12b7f71edc
Rob Hess <robwhess@gmail.com>
2020-05-18 10:02:02 -0700

    Add code from 5/18 lecture.

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+// app.get();
+
+// app.post();
+
+app.listen(8000, function () {
+  console.log("== Server is listening on port 8000!!!!!");
+});
