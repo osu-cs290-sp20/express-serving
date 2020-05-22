be475d0f8f9f5d6dce5b239a1ed750667700f677
Rob Hess <robwhess@gmail.com>
2020-05-22 09:53:59 -0700

    Add code from 5/22 lecture.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+var logger = require('./logger');
+
+app.use(logger);
+
+app.use(express.static(__dirname + "/public"));
+
 app.get('/about', function (req, res, next) {
   res.status(200);
   res.send("<html><body><h1>About page</h1></body></html>");
@@ -16,12 +22,23 @@ app.get('/people', function (req, res, next) {
   res.status(200).sendFile(__dirname + "/public/people.html");
 });
 
+var availablePeople = [
+  'luke',
+  'leia',
+  'rey',
+  'finn',
+  'r2d2'
+];
 app.get('/people/:name', function (req, res, next) {
   console.log("== req.params:", req.params);
   var name = req.params.name;
-  res.status(200).sendFile(
-    __dirname + "/public/people/" + name + ".html"
-  );
+  if (availablePeople.indexOf(name) >= 0) {
+    res.status(200).sendFile(
+      __dirname + "/public/people/" + name + ".html"
+    );
+  } else {
+    next();
+  }
 });
 
 app.get('/people/:name/:photo', function (req, res, next) {
